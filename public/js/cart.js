06757324088ab3f0fa513f2ee0e5f4a41fce2b29
ledhieus <ledan0602@gmail.com>
2024-07-11 10:44:47 +0700

    tạo giỏ hàng, thanh toan

---
+++ public/js/cart.js
@@ -0,0 +1,17 @@
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const inputsQuantity = document.querySelectorAll("input[name='quantity']")
+if(inputsQuantity.length > 0){
+    inputsQuantity.forEach(input => {
+        input.addEventListener("change", (e)=>{
+            const productId = input.getAttribute("product-id")
+            const quantity = input.value
+            // console.log(e.target.value)
+            // console.log(productId)
+            // console.log(quantity)
+            
+            //Chuyển hướng web
+            window.location.href = `/cart/update/${productId}/${quantity}`
+        })
+    })
+}
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -5,6 +5,7 @@ const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 
 
 
@@ -19,6 +20,9 @@ module.exports = (app)=>{
     app.use("/search", searchRoutes);
 
     app.use("/cart", cartRoutes);
+
+    app.use("/checkout", checkoutRoutes);
+
 
 
 }
